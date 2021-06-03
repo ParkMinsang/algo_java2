619a80a5cf2b3551787dff60b21317edd81b0ffa
minsang <whsrpdla@naver.com>
2021-06-04 01:11:19 +0900

    boj 13902

---
+++ src/boj/boj13902_2.java
@@ -0,0 +1,64 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+public class boj13902_2{
+    static final int INF = 999999;
+    static int n,m;
+    static int[] dp, dogu, dogu2;
+    static Set<Integer> woaks;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        dp = new int[n+1];
+        int[] woak = new int[m];
+        woaks = new HashSet<>();
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=0; i<m; i++){
+            woak[i] = Integer.parseInt(st.nextToken());
+            woaks.add(woak[i]);
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<m; j++){
+                if(i==j) continue;
+                woaks.add(woak[i]+woak[j]);
+            }
+        }
+
+        dogu = new int[woaks.size()];
+        int i=0;
+        for(int k : woaks){
+            dogu[i] = k;
+            i++;
+        }
+        Arrays.sort(dogu);
+
+        dogu2 = new int[dogu.length];
+        for(i=0; i<dogu.length; i++){
+            dogu2[i] = dogu[dogu.length-i-1];
+        }
+
+        int ans = func(n);
+        System.out.println(ans==INF?-1:ans);
+    }
+    static int func(int dishes){
+        if(dishes==0) return 0;
+        else if(dishes<0) return INF;
+
+        if(dp[dishes] != 0) return dp[dishes];
+
+        int result = INF;
+        for(int k : dogu2){
+            result = Math.min(result, func(dishes-k)+1);
+        }
+
+        dp[dishes] = result;
+        return dp[dishes];
+    }
+}
+
