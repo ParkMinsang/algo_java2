42a4cce55c22c1c2dcb7ae7721fd29d711f1ed57
minsang <whsrpdla@naver.com>
2021-06-13 00:00:59 +0900

    boj 2352

---
+++ src/boj/boj2352.java
@@ -0,0 +1,25 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+public class boj2352 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i=0; i<n; i++) nums[i]=Integer.parseInt(st.nextToken());
+
+        int[] LIS = new int[n];
+        int size=0;
+
+        for(int i=0; i<n; i++){
+            int idx = Arrays.binarySearch(LIS, 0, size, nums[i]);
+            idx = -(idx+1);
+            LIS[idx]=nums[i];
+            if(idx==size) size++;
+        }
+        System.out.println(size);
+    }
+}
