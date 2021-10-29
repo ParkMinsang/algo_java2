b79d839472470d24d8c4682f386d7556e98b4734
MinsangPark <whsrpdla@naver.com>
2021-10-30 00:59:20 +0900

    boj 2225 dp

---
+++ src/boj/boj2225.java
@@ -0,0 +1,39 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj2225 {
+    static final int MOD = 1000000000;
+    static int n,k;
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        dp = new int[n+1][k+1];
+//        for(int i=0; i<n+1; i++) Arrays.fill(dp[i], -1);
+
+        System.out.println(func(0, 0));
+    }
+    static int func(int num, int cnt){
+        if(num>n || cnt>k) return 0;
+        if(num==n && cnt==k) return 1;
+        if(dp[num][cnt]!=0) return dp[num][cnt];
+
+        int ret=0;
+        for(int i=0; i<=n; i++){
+            ret = (ret + func(num+i, cnt+1))%MOD;
+        }
+        dp[num][cnt]=ret;
+        return dp[num][cnt];
+    }
+
+
+}
